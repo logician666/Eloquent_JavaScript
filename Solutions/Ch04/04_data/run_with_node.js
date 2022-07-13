46536571f4dd25acc90922704f68da3d5c74e3a1
logician <logician666@outlook.com>
2022-07-13 04:15:10 +0200

    Rewritten a single line more concisely.

--- Solutions/Ch04/04_data/run_with_node.js
+++ Solutions/Ch04/04_data/run_with_node.js
@@ -3,7 +3,8 @@ require("./code/load")("code/journal.js", "code/chapter/04_data.js");
 
 for (let event of journalEvents(JOURNAL)) {
   let correlation = phi(tableFor(event, JOURNAL));
-  if (correlation > 0.1 || correlation < -0.1) {
+  // if (correlation > 0.1 || correlation < -0.1) {
+  if (Math.abs(correlation) > 0.1) {
     console.log(event + ":", correlation);
   }
 }
